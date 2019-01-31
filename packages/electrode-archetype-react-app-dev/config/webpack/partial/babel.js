cfd6f34b88623c882ad668d6d4d18624d4655522
divyamnpll <divyamnpll@gmail.com>
2019-01-31 13:14:04 -0800

    Update babel.js

--- packages/electrode-archetype-react-app-dev/config/webpack/partial/babel.js
+++ packages/electrode-archetype-react-app-dev/config/webpack/partial/babel.js
@@ -13,10 +13,62 @@ module.exports = function(options) {
 
   const clientVendor = Path.join(AppMode.src.client, "vendor/");
   const babelExclude = x => {
+    if (x.indexOf("cart-components-container-ny") >= 0) return false;
+    if (x.indexOf("cart-components-pos-ny") >= 0) return false;
+    if (x.indexOf("cart-components-core") >= 0) return false;
+    if (x.indexOf("order-summary-ny") >= 0) return false;
+    if (x.indexOf("feds-ny-core") >= 0) return false;
+    if (x.indexOf("feds-wmt-header") >= 0) return false;
+    if (x.indexOf("feds-wmt-footer") >= 0) return false;
+    if (x.indexOf("wmreact-base") >= 0) return false;
+    if (x.indexOf("wmreact-analytics") >= 0) return false;
+    if (x.indexOf("wmreact-tempo-analytics-utils") >= 0) return false;
+    if (x.indexOf("wmreact-interactive") >= 0) return false;
+    if (x.indexOf("transaction-analytics-core") >= 0) return false;
+    if (x.indexOf("wmreact-layout") >= 0) return false;
+    if (x.indexOf("feds-nuka-carousel") >= 0) return false;
+    if (x.indexOf("wmreact-modal") >= 0) return false;
+    if (x.indexOf("cc-banner-ny") >= 0) return false;
+    if (x.indexOf("wmreact-shipping-threshold-bar-ny") >= 0) return false;
     if (x.indexOf("node_modules") >= 0) return true;
     if (x.indexOf(clientVendor) >= 0) return true;
     return false;
   };
+
+   //console.log("logging the options in babeljs");
+   //console.log(JSON.stringify(options.babel, null, 4));
+   options.babel = {
+     "plugins": [
+       "syntax-dynamic-import",
+       ["babel-plugin-transform-object-rest-spread", {
+        "useBuiltIns": true
+      }],
+      "lodash"
+     ],
+     "env": {
+       "node" : {
+         "plugins": ["dynamic-import-node"]
+       }
+     },
+     "presets": [
+       ["env", {
+         "loose": true,
+         "useBuiltIns": true,
+         "targets": {
+           "browsers": ["> 5%"]
+         },
+         "debug": true,
+         "exclude": [
+           "transform-regenerator",
+           "es7.string.pad-start",
+           "es7.string.pad-end"
+         ]
+       }], "react", "flow"
+     ]
+
+   };
+   options.babel.babelrc = false;
+
 
   const babelLoader = {
     _name: "babel",
@@ -25,6 +77,7 @@ module.exports = function(options) {
     use: [
       {
         loader: "babel-loader",
+
         options: Object.assign(
           { cacheDirectory: Path.resolve(".etmp/babel-loader") },
           options.babel
